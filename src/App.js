1d55434fb1f1d9459b566d72022e816a9f92a7fb
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-05 13:18:43 +0600

    started routing practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
+  const router= createBrowserRouter([
+ {
+  path:'/',
+  element:<h1>Hello from root</h1>
+ },
+ {
+  path:'/home',
+  element:<h1>Hello from home</h1>
+ }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello</h1>
+    <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
